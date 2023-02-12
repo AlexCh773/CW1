0498f6ebd357e482f0fd5ef9e59ace53a88f3e67
AlexCh773 <achumichev@bk.ru>
2023-02-12 12:08:56 +0300

    практическая задача из билета №5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Book[] books = new Book[3];
+        books[0] = new Book("Война и мир", "Л.Н. Толстой");
+        books[1] = new Book("Чайка", "А.П. Чехов");
+        books[2] = new Book("Братья Карамазовы", "Ф.М. Достоевский");
 
+        for (Book book : books) {
+            System.out.println(book.getName().trim() + " - " + book.getAuthor().trim());
+        }
     }
 }
